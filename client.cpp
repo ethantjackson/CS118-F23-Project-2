d45e655256c48e6af78b0ae13fcc5f61aa93267e
ethantjackson <ethantjackson44@gmail.com>
2023-12-09 13:27:28 -0800

    modify closing handshake

--- client.cpp
+++ client.cpp
@@ -15,7 +15,8 @@
 #include "CongestionController.h"
 #include "utils.h"
 
-void packageFile(std::ifstream &fp, std::vector<packet> &packetVec) {
+void packageFile(std::ifstream &fp, std::vector<packet> &packetVec)
+{
     char buff[1024];
     unsigned short i = 0;
 
@@ -23,7 +24,8 @@ void packageFile(std::ifstream &fp, std::vector<packet> &packetVec) {
     long length = fp.tellg();
     fp.seekg(0, std::ios::beg);
 
-    while (fp.tellg() != length) {
+    while (fp.tellg() != length)
+    {
         packet pack;
         int toRead = std::min(int(length - fp.tellg()), 1024);
         fp.read(buff, toRead);
@@ -31,10 +33,10 @@ void packageFile(std::ifstream &fp, std::vector<packet> &packetVec) {
         packetVec.push_back(pack);
         i++;
     }
-    packetVec[packetVec.size() - 1].last = 1;
 }
 
-struct pendingPacketNode {
+struct pendingPacketNode
+{
     // packNum -> index in sender packetList
     int packNum;
     timeval sentTime;
@@ -42,13 +44,13 @@ struct pendingPacketNode {
     pendingPacketNode(int num, timeval tv) : packNum(num), sentTime(tv) {}
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int listen_sockfd, send_sockfd;
-    // struct sockaddr_in client_addr, server_addr_to, server_addr_from;
     struct sockaddr_in client_addr, server_addr_to;
 
-    // read filename from command line argument
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: ./client <filename>\n");
         return 1;
     }
@@ -56,14 +58,16 @@ int main(int argc, char *argv[]) {
 
     // Create a UDP socket for listening
     listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (listen_sockfd < 0) {
+    if (listen_sockfd < 0)
+    {
         perror("Could not create listen socket");
         return 1;
     }
 
     // Create a UDP socket for sending
     send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (send_sockfd < 0) {
+    if (send_sockfd < 0)
+    {
         perror("Could not create send socket");
         return 1;
     }
@@ -73,7 +77,8 @@ int main(int argc, char *argv[]) {
     timeout.tv_usec = 10;
 
     if (setsockopt(listen_sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout,
-                   sizeof timeout) < 0) {
+                   sizeof timeout) < 0)
+    {
         perror("setsockopt failed\n");
         return 1;
     }
@@ -92,7 +97,8 @@ int main(int argc, char *argv[]) {
 
     // Bind the listen socket to the client address
     if (bind(listen_sockfd, (struct sockaddr *)&client_addr,
-             sizeof(client_addr)) < 0) {
+             sizeof(client_addr)) < 0)
+    {
         perror("Bind failed");
         close(listen_sockfd);
         return 1;
@@ -100,7 +106,8 @@ int main(int argc, char *argv[]) {
 
     // Open file for reading
     std::ifstream fp(filename);
-    if (!fp.is_open()) {
+    if (!fp.is_open())
+    {
         perror("Error opening file");
         close(listen_sockfd);
         close(send_sockfd);
@@ -108,7 +115,6 @@ int main(int argc, char *argv[]) {
     }
 
     // RDT parameters
-    // const long long kTimeoutMicro = 250000;
     const long long kTimeoutMicro = 0.8 * 1000000;
     // RDT sender state
     int numDupes = 0;
@@ -122,7 +128,8 @@ int main(int argc, char *argv[]) {
     CongestionController congestionController(ssthresh);
 
     timeval sentTime;
-    if (gettimeofday(&sentTime, nullptr) != 0) {
+    if (gettimeofday(&sentTime, nullptr) != 0)
+    {
         std::cerr << "Error getting the current time" << std::endl;
         return -1;
     }
@@ -130,71 +137,71 @@ int main(int argc, char *argv[]) {
     pendingQueue.push_back(pendingPacketNode(0, sentTime));
 
     sendPacket(send_sockfd, &server_addr_to, packetList[0]);
-    while (true) {
+    while (!pendingQueue.empty())
+    {
         packet recvPack;
         bool isPresent;
         std::tie(recvPack, isPresent) = readPacket(listen_sockfd);
 
         // check for timeouts
-        if (!isPresent) {
+        if (!isPresent)
+        {
             timeval currTime;
-            if (gettimeofday(&currTime, nullptr) != 0) {
+            if (gettimeofday(&currTime, nullptr) != 0)
+            {
                 std::cerr << "Error getting the current time" << std::endl;
                 return -1;
             }
-            for (auto &pendingPack : pendingQueue) {
+            for (auto &pendingPack : pendingQueue)
+            {
                 long long timeElapsedMicro =
                     getTimeElapsed(pendingPack.sentTime, currTime);
 
-                if (timeElapsedMicro >= kTimeoutMicro) {
+                if (timeElapsedMicro >= kTimeoutMicro)
+                {
                     pendingPack.sentTime = currTime;
-                    // std::cout << "timeout: " << pendingPack.packNum
-                    //           << std::endl;
                     sendPacket(send_sockfd, &server_addr_to,
                                packetList[pendingPack.packNum]);
                     numDupes = 0;
                     cwnd = congestionController.gotTimeout();
                 }
             }
-        } else {
-            if (recvPack.last == 1) {
-                sendPacket(
-                    send_sockfd, &server_addr_to,
-                    packet{
-                        uint16_t(packetList.size() % 65535), 1, 1, 1, 0, {}});
-                break;  // do handshake stuff
-            }
+        }
+        else
+        {
             int recvAck = recvPack.acknum;
-            if (recvAck == pendingQueue.front().packNum) {
+            if (recvAck == pendingQueue.front().packNum)
+            {
                 cwnd = congestionController.gotDupAck();
                 numDupes += 1;
-                // std::cout << "numDupes: " << numDupes << std::endl;
-                if (numDupes == 3) {
-                    // std::cout
-                    //     << "fast retransmit: " <<
-                    //     pendingQueue.front().packNum
-                    //     << std::endl;
+                if (numDupes == 3)
+                {
                     sendPacket(send_sockfd, &server_addr_to,
                                packetList[pendingQueue.front().packNum]);
                 }
-            } else if (recvAck > pendingQueue.front().packNum) {
+            }
+            else if (recvAck > pendingQueue.front().packNum)
+            {
                 int initialCwnd = cwnd;
                 cwnd = congestionController.gotNewAck();
                 numDupes = 0;
                 int initialPackNum = pendingQueue.front().packNum;
                 for (int i = initialPackNum + initialCwnd; i < recvAck + cwnd;
-                     ++i) {
-                    if (i >= packetList.size()) {
+                     ++i)
+                {
+                    if (i >= packetList.size())
+                    {
                         break;
                     }
-                    if (packetWasSent[i]) {
+                    if (packetWasSent[i])
+                    {
                         continue;
                     }
                     packetWasSent[i] = true;
-                    // std::cout << "sending packet: " << i << std::endl;
                     sendPacket(send_sockfd, &server_addr_to, packetList[i]);
                     timeval sentTime;
-                    if (gettimeofday(&sentTime, nullptr) != 0) {
+                    if (gettimeofday(&sentTime, nullptr) != 0)
+                    {
                         std::cerr << "Error getting the current time"
                                   << std::endl;
                         return -1;
@@ -202,10 +209,53 @@ int main(int argc, char *argv[]) {
                     pendingQueue.push_back(pendingPacketNode(i, sentTime));
                     usleep(10000);
                 }
-                for (int i = initialPackNum; i < recvAck; ++i) {
+                for (int i = initialPackNum; i < recvAck; ++i)
+                {
                     pendingQueue.pop_front();
                 }
             }
+        }
+    }
+
+    sendPacket(
+        send_sockfd, &server_addr_to,
+        packet{
+            uint16_t(packetList.size() % 65535), 1, 1, 1, 0, {}});
+    if (gettimeofday(&sentTime, nullptr) != 0)
+    {
+        std::cerr << "Error getting the current time" << std::endl;
+        return -1;
+    }
+    while (true)
+    {
+        packet recvPack;
+        bool isPresent;
+        std::tie(recvPack, isPresent) = readPacket(listen_sockfd);
+
+        if (!isPresent)
+        {
+            timeval timeNow;
+            if (gettimeofday(&timeNow, nullptr) != 0)
+            {
+                std::cerr << "Error getting the current time" << std::endl;
+                return -1;
+            }
+            if (getTimeElapsed(sentTime, timeNow) > kTimeoutMicro)
+            {
+                sendPacket(send_sockfd, &server_addr_to,
+                           packet{
+                               uint16_t(packetList.size() % 65535), 1, 1, 1, 0, {}});
+                if (gettimeofday(&sentTime, nullptr) != 0)
+                {
+                    std::cerr << "Error getting the current time" << std::endl;
+                    return -1;
+                }
+            }
+            continue;
+        }
+        if (recvPack.last == 1)
+        {
+            break;
         }
     }
     fp.close();
--- server.cpp
+++ server.cpp
@@ -11,32 +11,31 @@
 
 #include "utils.h"
 
-void writeToFile(std::ofstream &fp, std::vector<packet> &packets) {
-    for (packet p : packets) {
+void writeToFile(std::ofstream &fp, std::vector<packet> &packets)
+{
+    for (packet p : packets)
+    {
         fp.write(p.payload, p.length);
     }
 }
 
-int main() {
+int main()
+{
     int listen_sockfd, send_sockfd;
-    // struct sockaddr_in server_addr, client_addr_from, client_addr_to;
     struct sockaddr_in server_addr, client_addr_to;
-    // struct packet buffer;
-    // socklen_t addr_size = sizeof(client_addr_from);
-    // int expected_seq_num = 0;
-    // int recv_len;
-    // struct packet ack_pkt;
 
     // Create a UDP socket for sending
     send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (send_sockfd < 0) {
+    if (send_sockfd < 0)
+    {
         perror("Could not create send socket");
         return 1;
     }
 
     // Create a UDP socket for listening
     listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (listen_sockfd < 0) {
+    if (listen_sockfd < 0)
+    {
         perror("Could not create listen socket");
         return 1;
     }
@@ -46,7 +45,8 @@ int main() {
     timeout.tv_usec = 10;
 
     if (setsockopt(listen_sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout,
-                   sizeof timeout) < 0) {
+                   sizeof timeout) < 0)
+    {
         perror("setsockopt failed\n");
         return 1;
     }
@@ -59,7 +59,8 @@ int main() {
 
     // Bind the listen socket to the server address
     if (bind(listen_sockfd, (struct sockaddr *)&server_addr,
-             sizeof(server_addr)) < 0) {
+             sizeof(server_addr)) < 0)
+    {
         perror("Bind failed");
         close(listen_sockfd);
         return 1;
@@ -76,89 +77,58 @@ int main() {
     std::vector<bool> receivedPacks;
     unsigned short cumAck = 0;
     timeval lastAckTv;
-    const long long kAckWaitMicro = 1600000;
 
-    while (true) {
+    while (true)
+    {
         packet recvPack;
         bool isPresent;
         std::tie(recvPack, isPresent) = readPacket(listen_sockfd);
-        // packet recvPack = std::get<0>(retval);
-        // bool isPresent = std::get<1>(retval);
 
-        if (!isPresent) continue;
-        for (int i = receivedPacks.size(); i <= recvPack.seqnum; ++i) {
+        if (!isPresent)
+            continue;
+        for (int i = receivedPacks.size(); i <= recvPack.seqnum; ++i)
+        {
             packetList.push_back(packet{});
             receivedPacks.push_back(false);
         }
         packetList[recvPack.seqnum] = recvPack;
         receivedPacks[recvPack.seqnum] = true;
-        while (cumAck < receivedPacks.size() && receivedPacks[cumAck]) {
+        while (cumAck < receivedPacks.size() && receivedPacks[cumAck])
+        {
             cumAck += 1;
         }
-        if (recvPack.last == 1) {
-            if (gettimeofday(&lastAckTv, nullptr) != 0) {
+        if (recvPack.last == 1)
+        {
+            if (gettimeofday(&lastAckTv, nullptr) != 0)
+            {
                 std::cerr << "Error getting the current time" << std::endl;
                 return -1;
             }
-            // std::cout << "resend last ack\n";
             sendPacket(send_sockfd, &client_addr_to,
                        packet{999, cumAck, 1, 1, 0, {}});
             break;
         }
-        // std::cout << "sending cum ack " << cumAck << std::endl;
         sendPacket(send_sockfd, &client_addr_to,
                    packet{999, cumAck, 1, 0, 0, {}});
     }
 
     timeval timeNow;
-    if (gettimeofday(&timeNow, nullptr) != 0) {
+    if (gettimeofday(&timeNow, nullptr) != 0)
+    {
         std::cerr << "Error getting the current time" << std::endl;
         return -1;
     }
 
-    // wait to verify the last packet is not resent (ie. the last ACK was
-    // dropped)
-    while (true) {
-        packet recvPack;
-        bool isPresent;
-        std::tie(recvPack, isPresent) = readPacket(listen_sockfd);
-
-        if (!isPresent) {
-            timeval timeNow;
-            if (gettimeofday(&timeNow, nullptr) != 0) {
-                std::cerr << "Error getting the current time" << std::endl;
-                return -1;
-            }
-            if (getTimeElapsed(lastAckTv, timeNow) > kAckWaitMicro) {
-                if (gettimeofday(&lastAckTv, nullptr) != 0) {
-                    std::cerr << "Error getting the current time" << std::endl;
-                    return -1;
-                }
-                sendPacket(send_sockfd, &client_addr_to,
-                           packet{999, cumAck, 1, 1, 0, {}});
-            }
-        }
-        if (recvPack.ack == 1) break;
-
-        if (getTimeElapsed(lastAckTv, timeNow) > kAckWaitMicro) {
-            if (gettimeofday(&lastAckTv, nullptr) != 0) {
-                std::cerr << "Error getting the current time" << std::endl;
-                return -1;
-            }
-            sendPacket(send_sockfd, &client_addr_to,
-                       packet{999, cumAck, 1, 1, 0, {}});
-        }
-    }
     // Open the target file for writing (always write to output.txt)
     std::ofstream fp("output.txt", std::ios_base::binary);
-    if (!fp.is_open()) {
+    if (!fp.is_open())
+    {
         perror("Error opening file");
         close(listen_sockfd);
         close(send_sockfd);
         return 1;
     }
 
-    // std::cout << "writing to file\n";
     writeToFile(fp, packetList);
 
     fp.close();
